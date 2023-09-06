cdc4ff1bca0520c821269c99d2e16a5f72999ae1
Ryan Haack <ryan@haackr.dev>
2023-09-06 16:15:24 -0600

    fix line item delete url and add some error handling

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ const environment = eb.Environments.US1;
 
   const budgetLine = {
     portalId: "69e94efa-9e63-4a87-a281-fc28c1a54e39",
-    itemId: "40f00380-b88a-46ed-8685-59d15a9f13d5",
+    itemId: "0eeb6344-111e-49bd-9f92-233182f3f67e",
     budgetId: "ff83414a-a8f7-4e20-bf8f-a2dbc9abcf9a",
   };
 
   await eb.deleteBudgetItem({ ...options, ...budgetLine });
+  await eb.deleteBudgetItem({
+    ...options,
+    portalId: "fake",
+    budgetId: "fake",
+    itemId: "fake",
+  });
 
   // await eb.logout({ ...options });
 })();
